4ee9149f96aee2ed2d8e706d9972851b031aa108
MurphyNeo <MurphyNeo@163.com>
2016-02-28 22:15:49 +0800

    1
    1

--- javascripts/titles.js
+++ javascripts/titles.js
@@ -3,12 +3,29 @@ $(function(){
         url: 'titles.txt',
         dataType: 'text',
         success: function(data) {
+        	data = data.replace(/.+/g,',');
             var arr = data.split(',');
             var liView = "";
             for(var i=0;i<arr.length;i++){
-                liView += '<li><a href="#">'+arr[i]+'</a></li>';
+                liView += '<li><a href="#" onclick="bloglist('+"'"+arr[i]+"'"+')">'+arr[i]+'</a></li>';
             }
             $("#titles").html(liView);
         }
     });
 });
+
+function bloglist(title){
+	$.ajax({
+        url: 'titles-'+title+'.txt',
+        dataType: 'text',
+        success: function(data) {
+        	data = data.replace(/.+/g,',');
+            var arr = data.split(',');
+            var liView = "";
+            for(var i=0;i<arr.length;i++){
+                liView += '<li><a href="/shell-string-operations/">'+arr[i]+'</a></li>';
+            }
+            $("#content").html(liView);
+        }
+    });
+}
