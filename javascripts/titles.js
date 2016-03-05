505e3a6ace6215e0468cb6146acfa20e21f2eea8
MurphyNeo <MurphyNeo@163.com>
2016-03-05 23:46:45 +0800

    1
    1

--- javascripts/titles.js
+++ javascripts/titles.js
@@ -84,16 +84,16 @@ function bloglist(title, number){
                     pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(parseInt(number)+1)+"'"+')">&gt; </a>';
                 }else if(number == countPage){
                     pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(number-1)+"'"+')">&lt; </a>';
-                    pageView += '<a href="javascript:void(0);" class="current">1</a>';
+                    pageView += '<a href="javascript:void(0);">1</a>';
                     pageView += '<a href="javascript:void(0);">...</a>';
                     pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(countPage-2)+"'"+')">'+(countPage-2)+'</a>';
                     pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(countPage-1)+"'"+')">'+(countPage-1)+'</a>';
-                    pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+countPage+"'"+')">'+countPage+'</a>';
+                    pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+countPage+"'"+')" class="current">'+countPage+'</a>';
                     pageView += '<a href="javascript:void(0);" class="disabled">&gt; </a>';
                 }else{
                     pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(number-1)+"'"+')">&lt; </a>';
                     if(number < 4){
-                        for(var i=1;i<6;i++){
+                        for(var i=1;i<5;i++){
                             if(i==number){
                                 pageView += '<a href="javascript:void(0);" class="current">'+i+'</a>';
                             }else{
@@ -102,7 +102,7 @@ function bloglist(title, number){
                         }
                         pageView += '<a href="javascript:void(0);">...</a>';
                     }
-                    if(number > 4 && number < countPage - 3){
+                    if(number >= 4 && number <= countPage - 4){
                         pageView += '<a href="javascript:void(0);">...</a>';
                         pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(number-2)+"'"+')">'+(number-2)+'</a>';
                         pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(number-1)+"'"+')">'+(number-1)+'</a>';
@@ -111,9 +111,9 @@ function bloglist(title, number){
                         pageView += '<a href="javascript:void(0);" onclick="bloglist('+"'"+title+"',"+"'"+(parseInt(number)+2)+"'"+')">'+(parseInt(number)+2)+'</a>';
                         pageView += '<a href="javascript:void(0);">...</a>';
                     }
-                    if(number > countPage - 3){
+                    if(number > countPage - 4){
                         pageView += '<a href="javascript:void(0);">...</a>';
-                        for(var i=1;i<6;i++){
+                        for(var i=1;i<5;i++){
                             if(i==number){
                                 pageView += '<a href="javascript:void(0);" class="current">'+i+'</a>';
                             }else{
