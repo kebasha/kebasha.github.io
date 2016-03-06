e4dc8783e2bfd2b7a2869a5562484a03577b41b1
kebasha <kbasha@qq.com>
2016-03-06 19:22:33 +0800

    1
    1

--- javascripts/app.js
+++ javascripts/app.js
@@ -84,6 +84,6 @@ function getQueryString(name)
 	
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    alert(decodeURI(r));
-    if(r!=null)return  unescape(r[2]); return null;
+    var data = decodeURI(r);
+    if(data!=null)return  unescape(data[2]); return null;
 }
