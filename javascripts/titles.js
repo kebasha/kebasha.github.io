ff8586a128e61651a25689c9f8e6224584971ce7
kebasha <kbasha@qq.com>
2016-03-06 14:48:21 +0800

    oracle
    oracle

--- javascripts/titles.js
+++ javascripts/titles.js
@@ -148,6 +148,15 @@ function loadDocument(title, index){
 
 }
 
+function is_weixin(){ 
+    var ua = navigator.userAgent.toLowerCase(); 
+    if(ua.match(/MicroMessenger/i)=="micromessenger") { 
+        return true; 
+    } else { 
+        return false; 
+    } 
+}
+
 
 
 
