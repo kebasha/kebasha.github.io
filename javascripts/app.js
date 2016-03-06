0889404ba3254b8b263688acc9a8905984976c4e
kebasha <kbasha@qq.com>
2016-03-06 19:08:06 +0800

    1
    1

--- javascripts/app.js
+++ javascripts/app.js
@@ -81,7 +81,8 @@ function is_weixin(){
 
 function getQueryString(name)
 {
-     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-     var r = window.location.search.substr(1).match(reg);
-     if(r!=null)return  unescape(r[2]); return null;
+	var data = decodeURI(name);
+    var reg = new RegExp("(^|&)"+ data +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
 }
