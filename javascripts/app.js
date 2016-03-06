4d5a3c3555add0299df01b1c3be6fdd46c649760
kebasha <kbasha@qq.com>
2016-03-06 19:21:15 +0800

    1
    1

--- javascripts/app.js
+++ javascripts/app.js
@@ -84,6 +84,6 @@ function getQueryString(name)
 	
     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    alert(r);
+    alert(decodeURI(r));
     if(r!=null)return  unescape(r[2]); return null;
 }
