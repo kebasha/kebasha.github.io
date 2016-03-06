0a1cf54c3b75901097cf05c47884302fdb253603
kebasha <kbasha@qq.com>
2016-03-06 16:43:56 +0800

    1
    1

--- javascripts/app.js
+++ javascripts/app.js
@@ -78,3 +78,10 @@ function is_weixin(){
         return false; 
     } 
 }
+
+function getQueryString(name)
+{
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
